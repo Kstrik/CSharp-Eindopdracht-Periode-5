8d46b1195ae6a1040efa0ec6939b6c8b78d08e3c
LeviVlasblom <D168228@edu.rocwb.nl>
2019-10-06 13:18:23 +0200

    Auto stash before merge of "master" and "origin/master"

--- CSharp_Eindopdracht_Periode_5/Battleship/Game.cs
+++ CSharp_Eindopdracht_Periode_5/Battleship/Game.cs
@@ -53,14 +53,14 @@ namespace Battleship
             //gameObject1.Material = new DiffuseMaterial(new ImageBrush(new BitmapImage(new Uri(@"C:\Users\Kenley Strik\Desktop\gencar_blue.png", UriKind.Absolute))));
             //this.world.AddGameObject(gameObject1);
 
-            //GameObject gameObject2 = new GameObject(this);
-            //gameObject2.Position = new Vector3D(0, 0, 0);
-            //gameObject2.GeometryModel = ModelUtil.ConvertToGeometryModel3D(new OBJModelLoader().LoadModel(@"C:\Users\Kenley Strik\Desktop\cars.obj"));
-            //gameObject2.Material = new DiffuseMaterial(new ImageBrush(new BitmapImage(new Uri(@"C:\Users\Kenley Strik\Desktop\gencar_blue.png", UriKind.Absolute))));
-            //this.world.AddGameObject(gameObject2);
+            GameObject gameObject2 = new GameObject(this);
+            gameObject2.Position = new Vector3D(0, 0, 0);
+            gameObject2.GeometryModel = ModelUtil.ConvertToGeometryModel3D(new OBJModelLoader().LoadModel(@"C:\Users\Levi Vlasblom\Desktop\cars.obj"));
+            gameObject2.Material = new DiffuseMaterial(new ImageBrush(new BitmapImage(new Uri(@"C:\Users\Levi Vlasblom\Desktop\gencar_blue.png", UriKind.Absolute))));
+            this.world.AddGameObject(gameObject2);
 
-            Water water = new Water(this);
-            this.world.AddGameObject(water);
+            //Water water = new Water(this);
+            //this.world.AddGameObject(water);
         }
 
         private void InitiliazeThread()
